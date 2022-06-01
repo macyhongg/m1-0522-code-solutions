1b3c045e4490f375001fc4f939f6ea63f36e84a1
Macy Hong <macyhong@gmail.com>
2022-05-31 20:14:19 -0700

    Comleted JS Operators and Expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+// Task 1
+var width = 12;
+var height = 31;
+var area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+// Task 2
+var bill = 158;
+var payment = 100;
+var change = bill - payment;
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+// Task 3
+var quizzes = 80;
+var midterm = 72;
+var final = 97;
+var grade = (quizzes + midterm + final)/3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+//Task 4
+var firstName = 'Macy';
+var lastName = 'Hong';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+// Task 5
+var pH = 24;
+var isAcidic = pH < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+// Task 6
+var headCount = 222;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+// Task 7
+var motto = fullName + ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
