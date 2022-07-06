6adbab87642cd963e8e48bd7a780776065e169ae
Macy Hong <macyhong@gmail.com>
2022-07-05 20:27:38 -0700

    Swap Chars remaining

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,10 @@
 /* exported swapChars */
+
+function swapChars(firstIndex, secondIndex, string) {
+  var array = string.split('')
+  var placeholer = firstIndex;
+  let array[firstIndex] = array[secondIndex];
+  let array[secondIndex] = array[placeholer];
+  // string.replace(firstIndex, secondIndex);
+  // string.replace(secondIndex, placeholder)
+}
