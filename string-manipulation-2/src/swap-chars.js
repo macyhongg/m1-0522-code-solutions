128b0ef80756d64acd7326c57ad7400145d30046
Macy Hong <macyhong@gmail.com>
2022-07-05 21:14:21 -0700

    Completed String Manipulations 2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,10 +1,22 @@
 /* exported swapChars */
 
-function swapChars(firstIndex, secondIndex, string) {
-  var array = string.split('')
-  var placeholer = firstIndex;
-  let array[firstIndex] = array[secondIndex];
-  let array[secondIndex] = array[placeholer];
-  // string.replace(firstIndex, secondIndex);
-  // string.replace(secondIndex, placeholder)
+function swapChars(firstIndex, secondIndex, string){
+  var array = string.split('');
+  var result = [];
+  for (i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      // console.log(string[i], string[secondIndex])
+      result.push(string[secondIndex]);
+      // console.log(result)
+    } else if (i === secondIndex) {
+      // console.log(string[i], string[firstIndex])
+      result.push(string[firstIndex]);
+      // console.log(result)
+    } else {
+      result.push(string[i]);
+      // console.log(result)
+    }
+  }
+  var final = result.join('');
+  return final;
 }
